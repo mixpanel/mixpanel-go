8be82501900eb7426139438b9ac66eb2b225532a
Matthew Hoare <matthew.hoare@mixpanel.com>
2023-05-23 18:32:56 +0000

    min requirements

--- mixpanel_test.go
+++ mixpanel_test.go
@@ -8,29 +8,29 @@ import (
 
 func TestMixpanelOptions(t *testing.T) {
 	t.Run("eu residency", func(t *testing.T) {
-		mp := NewClient(0, "", "", EuResidency())
+		mp := NewClient("", EuResidency())
 		require.Equal(t, mp.apiEndpoint, euEndpoint)
 		require.Equal(t, mp.dataEndpoint, euDataEndpoint)
 	})
 	t.Run("service account", func(t *testing.T) {
-		mp := NewClient(0, "", "", SetServiceAccount("username", "secret"))
+		mp := NewClient("", SetServiceAccount("username", "secret"))
 		require.NotNil(t, mp.serviceAccount)
 		require.Equal(t, "username", mp.serviceAccount.Username)
 		require.Equal(t, "secret", mp.serviceAccount.Secret)
 	})
 
 	t.Run("set proxy", func(t *testing.T) {
-		mp := NewClient(0, "", "", ProxyApiLocation("https://localhost:8080"))
+		mp := NewClient("", ProxyApiLocation("https://localhost:8080"))
 		require.Equal(t, "https://localhost:8080", mp.apiEndpoint)
 	})
 	t.Run("debug http", func(t *testing.T) {
-		mp := NewClient(0, "", "", DebugHttpCalls())
+		mp := NewClient("", DebugHttpCalls())
 		require.True(t, mp.debugHttp)
 	})
 }
 
 func TestMixpanelNewEvent(t *testing.T) {
-	mp := NewClient(0, "", "")
+	mp := NewClient("")
 	event := mp.NewEvent("some event", EmptyDistinctID, nil)
 	require.NotNil(t, event.Properties)
 }
