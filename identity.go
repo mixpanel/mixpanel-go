5561e4172df0d1c2afee87ffb01f185335785e74
Matthew Hoare <41530775+matthoare013@users.noreply.github.com>
2023-05-25 18:52:32 -0500

    won't support that endpoint (#38)

--- identity.go
+++ identity.go
@@ -2,9 +2,6 @@ package mixpanel
 
 import (
 	"context"
-	"fmt"
-	"net/http"
-	"net/url"
 )
 
 const (
@@ -54,64 +51,4 @@ func (m *Mixpanel) Merge(ctx context.Context, distinctID1, distinctID2 string) e
 	}
 
 	return m.doPeopleRequest(ctx, payload, mergeEndpoint, formData, acceptPlainText(), applicationFormData(), m.useApiSecret())
-}
-
-type IdentityEvent struct {
-	*Event
-}
-
-func (m *Mixpanel) NewIdentityEvent(distinctID string, properties map[string]any, identifiedId, anonId string) *IdentityEvent {
-	event := m.NewEvent("$identify", distinctID, properties)
-	i := &IdentityEvent{
-		Event: event,
-	}
-	i.SetIdentifiedId(identifiedId)
-	i.SetAnonId(anonId)
-
-	return i
-}
-
-func (i *IdentityEvent) IdentifiedId() any {
-	return i.Properties["$identified_id"]
-}
-
-// A distinct_id to merge with the $anon_id.
-func (i *IdentityEvent) SetIdentifiedId(id string) {
-	i.Properties["$identified_id"] = id
-}
-
-// A distinct_id to merge with the $identified_id. The $anon_id must be UUID v4 format and not already merged to an $identified_id.
-func (i *IdentityEvent) SetAnonId(id string) {
-	i.Properties["$anon_id"] = id
-}
-
-func (i *IdentityEvent) AnonId(id string) {
-	i.Properties["$anon_id"] = id
-}
-
-type IdentityOptions struct {
-	Strict bool
-}
-
-func (m *Mixpanel) CreateIdentity(ctx context.Context, events []*IdentityEvent, identityOptions IdentityOptions) error {
-	// todo support import option
-	// todo add strict option
-
-	query := url.Values{}
-	query.Add("verbose", "1")
-
-	response, err := m.doRequestBody(
-		ctx,
-		http.MethodPost,
-		m.apiEndpoint+identityEndpoint,
-		events,
-		None,
-		addQueryParams(query), acceptPlainText(),
-	)
-	if err != nil {
-		return fmt.Errorf("failed to create identity:%w", err)
-	}
-	defer response.Body.Close()
-
-	return returnVerboseError(response)
 }
