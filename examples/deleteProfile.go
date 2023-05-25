187a8599dbbcafbca50ac796c4d1f2a82979b111
Matthew Hoare <41530775+matthoare013@users.noreply.github.com>
2023-05-24 19:18:36 -0500

    remove profile testing (#33)

---
+++ examples/deleteProfile.go
@@ -0,0 +1,38 @@
+package examples
+
+import (
+	"context"
+
+	"github.com/mixpanel/mixpanel-go"
+)
+
+func DeleteProfile() error {
+	ctx := context.Background()
+
+	mp := mixpanel.NewClient(
+		"token",
+		mixpanel.ProjectID(0),
+		mixpanel.ServiceAccount("user_name", "secret"),
+	)
+
+	// Can use the SetReversedProperty or make the reserved property yourself
+	spartan117 := mixpanel.NewPeopleProperties("Spartan-117", map[string]any{
+		string(mixpanel.PeopleFirstNameProperty): "John",
+		"ai":                                     "Cortana",
+	})
+
+	if err := mp.PeopleSet(ctx,
+		[]*mixpanel.PeopleProperties{
+			spartan117,
+		},
+	); err != nil {
+		return err
+	}
+
+	// Spartans never die. They're just missing in action
+	if err := mp.PeopleDeleteProfile(ctx, "Spartan-117", true); err != nil {
+		return err
+	}
+
+	return nil
+}
